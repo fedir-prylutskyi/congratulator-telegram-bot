13839638a79ea633c3c576642a191c70a8ce026c
fedir-prylutskyi <lWAJmN49Xdv6>
2021-06-27 00:50:39 +0300

    test prettier

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import { green } from "chalk";
 import { bot } from '../bot/bot';
 dotenv.config();
