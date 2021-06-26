6cf49a96db909e969252f8918ca5fc72029f6230
fedir-prylutskyi <lWAJmN49Xdv6>
2021-06-26 20:18:38 +0300

    add typescript

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,11 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const bot = require('./bot');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 // Set the bot API endpoint
-app.use(bot.webhookCallback('/secret-path'));
+// app.use(bot.webhookCallback('/secret-path'));
 
 app.listen(process.env.PORT || 5000, () => {
+    // tslint:disable-next-line:no-console
     console.log('App listening on port ' + process.env.PORT || 5000);
 });
